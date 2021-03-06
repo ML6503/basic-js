d110446d7772560d56a9b281ee51a898522defb8
ML6503 <ml6503@gmail.com>
2021-03-06 23:29:41 +0200

    feat: add solution to extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const repeat = (length, str, separator) => {
+  const array = [];
+  for (let i = 0; i < length; i++) {
+    i === length - 1 ? array.push(str) : array.push(str.concat(separator));
+  }
+
+  return array;
+};
+
+const convertToString = (val) =>  typeof str !== 'string' ? String(val) : val;
+
+module.exports = function repeater(str, options) {
+  str = convertToString(str);
+
+  let additionArray;
+
+  const createAddstr = () => {
+    const additionRepeat = options.additionRepeatTimes
+      ? options.additionRepeatTimes
+      : 1;
+    additionArray = repeat(
+      additionRepeat,
+      convertToString(options.addition),
+      options.additionSeparator
+        ? convertToString(options.additionSeparator)
+        : "|"
+    );
+
+    return additionArray.join("");
+  };
+
+  const strRepeat = options.repeatTimes ? options.repeatTimes : 1;
+  const strArray = repeat(
+    strRepeat,
+    options.addition !== undefined ? str.concat(createAddstr()) : str,
+    options.separator ? convertToString(options.separator) : "+"
+  );
+
+  return strArray.join("");
 };
   

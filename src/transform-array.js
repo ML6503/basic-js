1945980c84cdf3434002adbc74ca0777f25ecc54
ML6503 <ml6503@gmail.com>
2021-03-08 21:34:31 +0200

    feat: add solution to transform array task, except 2 tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if(!Array.isArray(arr)) {
+    throw new Error("THROWN");
+}
+
+const arrCheck = [...arr];
+
+arrCheck.map((el, i) => {
+  if (el === "--double-prev") {
+    return i === 0 ? arrCheck.splice(i, 1) : arr[i - 1];
+  }
+  if (el === "--double-next") {    
+    return i === arr.length - 1 ? arrCheck.splice(i, 1) : arr[i + 1];
+  } else {
+    return el;
+  }
+});
+
+
+  const indexDN = arrCheck.indexOf("--discard-next");
+  if (indexDN > -1) {
+    if (indexDN === arrCheck.length - 1) {
+      arrCheck.splice(indexDN, 1);
+    } else {
+      arrCheck.splice(indexDN, 2);
+    }
+  }
+  const indexDP = arrCheck.indexOf("--discard-prev");
+
+  if (indexDP > -1) {
+   
+    if (indexDP === 0) {
+      arrCheck.splice(indexDP, 1);
+    } else {
+      arrCheck.splice(indexDP - 1, 2);
+    }
+  }
+
+  return arrCheck;
 };
